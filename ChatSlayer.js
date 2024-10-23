2a5b40755b05ccb834ca851206d0cbb66309f894
Thiago Costa Martins <thiagogames2611@gmail.com>
2024-10-23 14:12:11 -0400

    Add files via upload

--- ChatSlayer.js
+++ ChatSlayer.js
@@ -1,3 +1,29 @@
+let selectedText = document.getElementById('exemplo');
+
+function trocaDeValoresEscolhidos(selectedText){
+
+      text = selectedText.textContent;
+
+      let textArray = text.split("");
+
+      textArrayLength = textArray.length;
+
+
+
+      for(let i = 0; i<=textArrayLength; i++){
+          
+         
+
+      }
+
+      
+      text = textArray.join("")
+}
+
+
+function formula(){
+
+}
 
 function ChangeText(text){
 
@@ -12,13 +38,6 @@ function ChangeText(text){
       for(let i = 0; i<=textArrayLength; i++){
           
             if(isNaN(textArray[i])){
-
-                  if (textArray[i] === "i" || textArray[i] === "I") {
-                        if (textArray[i+1] === "g" && textArray[i+2] === "u" && textArray[i+3] === "a" && textArray[i+4] === "l") {
-                            // Substitui a palavra "igual" por "diferente"
-                            textArray.splice(i, 5, "d", "i", "f", "e", "r", "e", "n", "t", "e");
-                        }
-                  }
 
             }else if(textArray[i] != " "){
 
@@ -40,12 +59,15 @@ function ChangeText(text){
       
 }
 
-let selectedText = document.getElementById('exemplo');
+
 
 document.addEventListener('copy', function (event){
 
       event.preventDefault();
       event.clipboardData.setData('text/plain',  ChangeText(selectedText));
+
+
+
     
 
 });
